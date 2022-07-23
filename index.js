794e509bdf58d3d7675511e521782ff7d497b7a4
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-07-23 14:25:03 -0500

    Add procfile to directory

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ const unkownEndpoint = (req, res) => {
 app.use(unkownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
